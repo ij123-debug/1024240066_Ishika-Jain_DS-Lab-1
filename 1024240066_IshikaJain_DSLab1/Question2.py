9fa8aa586e917f2775234323fe127bf529191ed8
ij123-debug <ishikajains123@gmail.com>
2025-08-19 21:50:38 +0530

    Delete 1024240066_IshikaJain_DSLab1 directory

--- 1024240066_IshikaJain_DSLab1/Question2.py
+++
@@ -1,7 +0,0 @@
-arr = list(map(int, input("Enter elements separated by space: ").split()))
-unique_arr = []
-for x in arr:
-    if x not in unique_arr:
-        unique_arr.append(x)
-
-print(f"Array after removing duplicates: {unique_arr}")
--- 1024240066_IshikaJain_DSLab1/Question3.py
+++
@@ -1,3 +0,0 @@
-arr = [1] + [0] * 4
-for i in range(5):
-    print(arr[i], end="")
